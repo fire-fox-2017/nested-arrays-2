f68a600ba6fda7e2719ea5cb7bd60ea71a6b1ee5
fearzen3 <ivanhabi2@gmail.com>
2017-03-29 22:03:32 +0700

    selse

--- nested_array_2_convert.js
+++ nested_array_2_convert.js
@@ -13,7 +13,32 @@ let roster = [
 
 function convert_roster_format (nestedArray) {
   // your convert code here
+  let rosterObj ={
+    'number':'',
+    'name':'',
+    'position':'',
+    'pointsPerGame':''
+  }
+var arrRoster=[];
+for(let i=1;i<nestedArray.length;i++){
+  rosterObj ={
+    'number':'',
+    'name':'',
+    'position':'',
+    'pointsPerGame':''
+  }
+  rosterObj.number=nestedArray[i][0];
+  rosterObj.name=nestedArray[i][1];
+  rosterObj.position=nestedArray[i][2];
+  rosterObj.pointsPerGame=nestedArray[i][3];
+  arrRoster.push(rosterObj);
 }
+
+  return arrRoster;
+
+}
+
+console.log(convert_roster_format(roster));
 
 let object_roster = convert_roster_format(roster)
 console.log(object_roster[2])
@@ -21,6 +46,7 @@ console.log(object_roster[2])
 // => { "Number": 31, "Name": "Harvey Kay", "Position": "Shooting Guard", "Points per Game": [0, 30, 16, 0, 25] }
 
 console.log(object_roster[0]['Name'] == 'Joe Schmo') // outputs true
+console.log(object_roster[0].name == 'Joe Schmo')
 
 module.exports = {
   convert_roster_format
--- nested_array_2_tictactoe.js
+++ nested_array_2_tictactoe.js
@@ -1,5 +1,41 @@
 function tic_tac_toe_board () {
   // ...
+  let sisaX;
+  let sisaO;
+  let sisa=true;
+  let rand = Math.floor((Math.random() * 2));
+  if(rand==1){
+    sisaX=5;
+    sisaO=4;
+
+  }else{
+    sisaX=4;
+    sisaO=5;
+  }
+
+  var tampung=[];
+  let tampungBoard=[];
+
+  while (sisa) {
+    rand = Math.floor((Math.random() * 2));
+    if(rand==1 && sisaX>0){
+      sisaX--;
+      tampung.push('X');
+    }
+    if(rand==0 && sisaO>0){
+      sisaO--;
+      tampung.push('O');
+    }
+    if(sisaO===0&&sisaX===0){
+      sisa=false;
+    }
+  }
+
+  for(let i = 0;i <= 7;i=i+3 ){
+  //  console.log(i+" "+(i+2));
+    tampungBoard.push(tampung.slice(i,(i+3)));
+  }
+  return tampungBoard;
 }
 
 console.log(tic_tac_toe_board()) // => make a random tic tic board
