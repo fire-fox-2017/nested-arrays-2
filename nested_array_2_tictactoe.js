27d2868e4e3c50c76aacf330ed7327bf4a93101b
Rudy Wahjudi <rudy.wahjudi@gmail.com>
2017-03-29 12:04:26 +0800

    tictactoe now can populate 4x and 5o and the other way around

--- nested_array_2_tictactoe.js
+++ nested_array_2_tictactoe.js
@@ -1,20 +1,38 @@
 function tic_tac_toe_board () {
   // populate a board
   let board = [];
+  let countX = 0;
+  let countO = 0;
+
   for (let row = 0 ; row < 3 ; row++) {
     let column = [];
 
     for (let col = 0 ; col < 3 ; col++) {
       let random = Math.floor(Math.random() + 0.5);
-      console.log(random);
-      if (random)
-        column.push('X');
-      else
-        column.push('O');
+      // console.log(random);
+      if (random) {
+        if(countX == 5) {
+          column.push('O');
+          countO++;
+        } else {
+          column.push('X');
+          countX++;
+        }
+      } else {
+        if(countO == 5) {
+          column.push('X');
+          countX++;
+        } else {
+          column.push('O');
+          countO++;
+        }
+      }
     }
     board.push(column);
   }
 
+
+  console.log("X = " + countX + ", O = " + countO);
   return board;
 }
 
