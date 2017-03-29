a0bf887180a43f4bbd1843fd35fc060a4eac0c0a
Parel Wellman Hutahaean <parel.hutahaean@gmail.com>
2017-03-29 19:15:09 +1400

    Release 0 Tic-tac-toe Revisited Finished

--- nested_array_2_tictactoe.js
+++ nested_array_2_tictactoe.js
@@ -1,5 +1,23 @@
 function tic_tac_toe_board () {
-  // ...
+  let board = [];
+  let boardLength = 3;
+  // Menentukan banyak x dan y
+  let o = (Math.round(Math.random()) == 0) ? 5 : 4;
+  let x  = 9 - o;
+  for (let i = 0; i < boardLength; i++) {
+    board.push([]);
+    for (let j = 0; j < boardLength; j++){
+      if (Math.round(Math.random()) == 0) {
+        value = o > 0 ? "o" : "x";
+        o--;
+      } else {
+        value = x > 0 ? "x" : "o";
+        x--;
+      }
+      board[i].push(value);
+    }
+  }
+  return board;
 }
 
 console.log(tic_tac_toe_board()) // => make a random tic tic board
