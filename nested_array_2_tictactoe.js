cbfbf68c4fd16c8d9cfbce1740060955caed672d
Rudy Wahjudi <rudy.wahjudi@gmail.com>
2017-03-29 11:58:33 +0800

    tictactoe populated board

--- nested_array_2_tictactoe.js
+++ nested_array_2_tictactoe.js
@@ -1,5 +1,21 @@
 function tic_tac_toe_board () {
-  // ...
+  // populate a board
+  let board = [];
+  for (let row = 0 ; row < 3 ; row++) {
+    let column = [];
+
+    for (let col = 0 ; col < 3 ; col++) {
+      let random = Math.floor(Math.random() + 0.5);
+      console.log(random);
+      if (random)
+        column.push('X');
+      else
+        column.push('O');
+    }
+    board.push(column);
+  }
+
+  return board;
 }
 
 console.log(tic_tac_toe_board()) // => make a random tic tic board
