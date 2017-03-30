7ebcb404e4d711f6be920cf79aede7414b40646f
Didit Aditya <email": null>
2017-03-30 08:10:01 +0700

    Fixes the max limit

--- nested_array_2_tictactoe.js
+++ nested_array_2_tictactoe.js
@@ -13,23 +13,19 @@ function tic_tac_toe_board () {
 
   let checkCount = (index) => {
     if (index === 0) {
-      if (symbolCount[0] < minLimit) {
-        symbolCount[0] += 1;
-        return 0;
-      } else if (symbolCount[0] < maxLimit) {
+      if (symbolCount[0] < maxLimit) {
         symbolCount[0] += 1;
         return 0;
       } else {
+        symbolCount[1] += 1;
         return 1;
       }
     } else {
-      if (symbolCount[1] < minLimit) {
-        symbolCount[1] += 1;
-        return 1;
-      } else if (symbolCount[1] < maxLimit) {
+      if (symbolCount[1] < maxLimit) {
         symbolCount[1] += 1;
         return 1;
       } else {
+        symbolCount[0] += 1;
         return 0;
       }
     }
@@ -44,8 +40,10 @@ function tic_tac_toe_board () {
     board.push([]);
     for (let j = 0; j < col; j++) {
       board[i].push(symbol[randomize()]);
+      // console.log(symbolCount);
     }
   }
+
 
   return board;
 
