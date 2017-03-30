7e5fa720ed824b832984bfec3291b6ecae033939
arahito <uarahito@yahoo.com>
2017-03-30 07:37:28 +0700

    Nested Array 2

--- nested_array_2_tictactoe.js
+++ nested_array_2_tictactoe.js
@@ -1,5 +1,32 @@
 function tic_tac_toe_board () {
-  // ...
+  let tempTTTResult = [];
+  let limitX = 0, limitY = 0;
+  let tempRand;
+  for (var i = 0; i < 3; i++) {
+    let tempTTT= [];
+    for (var j = 0; j < 3; j++) {
+      tempRand = Math.floor(Math.random() * 2);
+      if (tempRand === 0) {
+        if (limitX <= 4) {
+          tempTTT.push('x');
+          limitX++;
+        } else {
+          tempTTT.push('o');
+        }
+      } else {
+        if (limitY <= 4) {
+          tempTTT.push('o');
+          limitY++;
+        } else {
+          tempTTT.push('x');
+        }
+      }
+      // tempTTT.push(j);
+    }
+    tempTTTResult.push(tempTTT);
+  }
+
+  return tempTTTResult;
 }
 
 console.log(tic_tac_toe_board()) // => make a random tic tic board
