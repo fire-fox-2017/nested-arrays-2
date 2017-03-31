5c70823bb602bd638a47a7c929fff4be8f08748e
1000piar <nunua7a8@gmail.com>
2017-03-31 19:55:27 +0700

    Array-2

--- nested_array_2_tictactoe.js
+++ nested_array_2_tictactoe.js
@@ -1,5 +1,39 @@
 function tic_tac_toe_board () {
   // ...
+function random_tic_tac(){
+  var good = 0;
+  var bad = 0;
+  var str = '';
+  var random = Math.floor(Math.random()*2);
+if(random === 0){
+  if(good > 4){
+    good++;
+    str='X';
+  }else {
+    return str= 'O'
+  }
+}else{
+  if(bad > 4){
+    bad++;
+    str= 'O';
+  }else{
+    str= 'X'
+  }
+}
+return str;
+
+}
+
+var board = [];
+for (var i =  0; i < 3; i ++){
+  var newArr = [];
+  board.push(newArr);
+  for (var j = 0 ; j < 3; j ++){
+    board[i].push(random_tic_tac());
+    // board[i];
+  }
+}
+return board;
 }
 
 console.log(tic_tac_toe_board()) // => make a random tic tic board
